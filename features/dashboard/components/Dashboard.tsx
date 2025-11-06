f80a4dd1f8dd6bca46389b53ed93c832855a681c
dlevenson44 <daniellevenson4@gmail.com>
2025-11-05 21:18:27 -0500

    render riskscore card on dashboard

--- features/dashboard/components/Dashboard.tsx
+++ features/dashboard/components/Dashboard.tsx
@@ -2,10 +2,21 @@
 
 import { useState } from 'react';
 
+import OrganizationOverview from './OrganizationOverview';
+import RiskScore from './RiskScore';
+import RiskScoreFactors from './RiskScoreFactors';
 import Header from '~/shared/components/Header/Header';
 import Sidebar from '~/shared/components/Sidebar/Sidebar';
 import SwitchFilter from '~/shared/components/Filters/SwitchFilter';
-
+import {
+  mockRiskScoreData,
+  // mockRiskScoreFactors,
+  // mockTimeSeriesData,
+  // mockOrganizationStats,
+  // mockSimulatedAttacks,
+  // mockAttackDetails,
+  // getTotalInteractions,
+} from "~/shared/utils/mockData";
 import { timeFilters } from '~/shared/utils/filters/time-filter';
 
 const Dashboard = () => {
@@ -26,6 +37,7 @@ const Dashboard = () => {
         {/* Main Dashboard Content */}
         <main className="flex-1 overflow-y-auto p-8 bg-white">
           <div className="mb-8">
+            {/* Filter */}
             <div className="flex items-center justify-between mb-6">
               <h2 className="text-2xl text-gray-900">Risk Insights</h2>
               <SwitchFilter
@@ -34,6 +46,12 @@ const Dashboard = () => {
                 handleClick={handleFilterClick}
               />
             </div>
+
+          {/* Top Row Cards */}
+          <div className="grid grid-cols-[30%_70%] lg:grid-cols-[30%_70%] gap-8 mb-6 mr-8">
+            <RiskScore data={mockRiskScoreData} />
+            {/* <RiskScoreFactors factors={mockRiskScoreFactors} /> */}
+          </div>
           </div>
         </main>
       </div>
